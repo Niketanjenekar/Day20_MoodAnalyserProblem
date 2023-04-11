dc3ffede135f8348538858b229045290701df8e0
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-04-11 23:26:44 +0530

    UseCase 1 of Mood Analyser

---
+++ MoodAnalyser_TestCase1/UnitTest1.cs
@@ -0,0 +1,46 @@
+using Day20_MoodAnalyserProblems;
+
+namespace MoodAnalyser_TestCase1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //Arrange
+            string expected = "Sad";
+            string message = "I am in sad mood";
+
+            MoodAnalyser_UC1 moodAnalyser = new MoodAnalyser_UC1(message);
+
+            //Act
+            string mood = moodAnalyser.AnalysetheMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood);
+
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            //Arrange
+            string expected = "Happy";
+            string message = "I am in any mood";
+
+            MoodAnalyser_UC1 moodAnalyser = new MoodAnalyser_UC1(message);
+
+            //Act
+            string mood = moodAnalyser.AnalysetheMood();
+            
+            //Assert
+            Assert.AreEqual(expected, mood);
+
+        }
+    }
+}
