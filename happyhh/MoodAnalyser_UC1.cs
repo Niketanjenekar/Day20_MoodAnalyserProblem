15cc0f4d151b59bb7c9e1eecc0673fbad67ebd82
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-04-12 11:40:13 +0530

    Happy

---
+++ happyhh/MoodAnalyser_UC1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20_MoodAnalyserProblems
+{
+    public class MoodAnalyser_UC1
+    {
+        private string message;
+
+        public MoodAnalyser_UC1(string message)
+        {
+            this.message = message;
+
+        }
+
+        public string AnalysetheMood()
+        {
+            if (message.Contains("any"))
+            {
+                return "Happy";
+            }
+            else if (message.Contains("happy"))
+            {
+                return "Happy";
+            }
+            else if (message.Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Can't Analyse your mood, i.e., Mood is Unkown";
+            }
+        }
+    }
+}
---
+++ happyhh/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day20_MoodAnalyserProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Problem");
+
+
+        }
+    }
+    
+}
